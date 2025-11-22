0641e4bc04be6b562696b09352c0c601706c5781
Ryan Reede <12736540+reedery@users.noreply.github.com>
2025-11-22 10:55:46 +0100

    add proj setup files

---
+++ realitykit-ragdollsTests/realitykit_ragdollsTests.swift
@@ -0,0 +1,17 @@
+//
+//  realitykit_ragdollsTests.swift
+//  realitykit-ragdollsTests
+//
+//  Created by ryan reede on 11/22/25.
+//
+
+import Testing
+@testable import realitykit_ragdolls
+
+struct realitykit_ragdollsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ realitykit-ragdollsUITests/realitykit_ragdollsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  realitykit_ragdollsUITestsLaunchTests.swift
+//  realitykit-ragdollsUITests
+//
+//  Created by ryan reede on 11/22/25.
+//
+
+import XCTest
+
+final class realitykit_ragdollsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
